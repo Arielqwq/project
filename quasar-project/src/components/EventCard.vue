8ceef766364843e9f9803a12f770b241c89b7966
Ariel <dawei4102@gmail.com>
2023-02-14 01:53:42 +0800

    0214
    event/homeview

--- quasar-project/src/components/EventCard.vue
+++ quasar-project/src/components/EventCard.vue
@@ -1,17 +1,32 @@
 <template lang="pug">
-.flex.wrap
-  .div
-    img(:src="image" cover style="height:150px")
-    q-card-section.q-pa-none
-      router-link.text-weight-bold.text.Underline-none(:to="'/events/' + _id") {{ title }}
-    q-card-section.q-pt-none
-      .text-subtitle 日期： {{ daysfrom }} ~ {{ daysto }}
-    q-card-section.q-pt-none
-      .text-subtitle 費用： ${{ price }}
-    q-card-actions(align='right')
-      q-btn(flat round color='red' icon='favorite')
-      q-btn(flat round color='teal' icon='fa-solid fa-cart-plus' @click="editCart({_id, quantity: 1, text:'加入成功'})")
-      q-btn(flat round color='primary' icon='share').
+//- .q-pa-md.row.items-start.q-gutter-md
+q-card.my-card(flat bordered)
+  q-card-section(horizontal)
+    q-card-section.q-pt-xs
+      .text-h5.q-mt-sm.q-mb-xs {{title}}
+      .text-caption.text-grey
+        p {{ description }}
+    q-card-section.col-5.flex.flex-center
+      q-img.rounded-borders(:src='image')
+  q-separator
+  q-card-actions
+    q-btn(flat round icon='event')
+    q-btn(flat)
+    q-btn(flat color='primary' label='Reserve')
+
+//- .flex.wrap
+//-   .div
+//-     img(:src="image" cover style="height:150px")
+//-     q-card-section.q-pa-none
+//-       router-link.text-weight-bold.text.Underline-none(:to="'/events/' + _id") {{ title }}
+//-     q-card-section.q-pt-none
+//-       .text-subtitle 日期： {{ daysfrom }} ~ {{ daysto }}
+//-     q-card-section.q-pt-none
+//-       .text-subtitle 費用： ${{ price }}
+//-     q-card-actions(align='right')
+//-       q-btn(flat round color='red' icon='favorite')
+//-       q-btn(flat round color='teal' icon='fa-solid fa-cart-plus' @click="editCart({_id, quantity: 1, text:'加入成功'})")
+//-       q-btn(flat round color='primary' icon='share').
 
 </template>
 
@@ -41,10 +56,10 @@ defineProps({
     type: String,
     default: ''
   },
-  // description: {
-  //   type: String,
-  //   default: ''
-  // },
+  description: {
+    type: String,
+    default: ''
+  },
   image: {
     type: String,
     default: ''
@@ -64,5 +79,8 @@ defineProps({
 </script>
 
 <style lang="sass">
+.my-card
+  width: 30%
+  max-width: 350px
 
 </style>
--- quasar-project/src/pages/front/HomeView.vue
+++ quasar-project/src/pages/front/HomeView.vue
@@ -8,20 +8,29 @@
           q-card(style="width:250px").flex.justify-center.q-pa-lg.col-12.col-md-6.col-lg-3
             ProductCard(v-bind="product")
 
+    .event-area.flex.q-ma-lg.justify-center.row.col-5
+      div.flex.row.wrap(v-for="event in events" :key="event._id")
+        q-card(style="width:250px").flex.justify-center.q-pa-lg.col-12.col-md-6.col-lg-3
+          EventCard(v-bind="event")
+
 </template>
 <script setup>
 import { reactive, ref, onMounted } from 'vue'
 import { api } from '@/boot/axios'
 import Swal from 'sweetalert2'
 import ProductCard from '@/components/ProductCard.vue'
+import EventCard from '@/components/EventCard.vue'
+
+// swiper
 import { SwiperSlide, Swiper } from 'swiper/vue'
 import { Navigation, Autoplay } from 'swiper'
-
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+// swiper
 
 const products = reactive([])
+const events = reactive([])
 
 const swiperOptions = {
   slidesPerView: 1,
@@ -51,6 +60,19 @@ const swiperOptions = {
   try {
     const { data } = await api.get('/products')
     products.push(...data.result)
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      title: '失敗',
+      text: error?.response?.data?.message || '發生錯誤'
+    })
+  }
+})();
+
+(async () => {
+  try {
+    const { data } = await api.get('/events')
+    events.push(...data.result)
   } catch (error) {
     Swal.fire({
       icon: 'error',
