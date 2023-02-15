bd0dbed6e4012ef0265a1235e420bac5b87fe5b2
Ariel <dawei4102@gmail.com>
2023-02-16 01:09:45 +0800

    0216

--- quasar-project/src/layouts/FrontLayout.vue
+++ quasar-project/src/layouts/FrontLayout.vue
@@ -9,15 +9,36 @@
         q-btn(v-if="!isLogin" push round icon="fa-solid fa-user" @click="showLogin = true")
         //- <q-btn v-if="!isLogin" font-awesome-icon icon="fa-solid fa-user" to="/register" label="註冊"/>
         //- <q-btn v-if="!isLogin" font-awesome-icon icon="fa-solid fa-right-to-bracket" to="/login" label="登入"/>
-        //- q-btn(flat rounded v-if="isLogin" icon="fa-solid fa-cart-shopping" to="/cart")
+        //- 購物車及數量
         q-btn(flat rounded v-if="isLogin && !isAdmin" icon="fa-solid fa-cart-shopping" to="/Mypage/Mypagecart")
           q-badge( color="red" :label="cart" rounded floating)
-        //- q-btn(flat rounded v-if="isLogin"  icon="fa-solid fa-list" to="/orders" label="訂單")
-        q-btn(flat rounded v-if="isLogin && !isAdmin"  icon="fa-solid fa-list" to="/Mypage/MypageOrders" label="訂單")
-        q-btn(flat rounded v-if="isLogin && isAdmin" icon="fa-solid fa-hammer" to="/admin" label="管理")
-        q-btn#logoutBtn(flat rounded v-if="isLogin" icon="fa-solid fa-right-from-bracket" @click="logout" label="登出")
 
-        //- q-btn(dense flat round icon="menu" @click="toggleLeftDrawer")
+        //- 會員登入後的下拉選單
+        q-btn-dropdown(v-if="isLogin && !isAdmin" flat rounded icon="fa-solid fa-user-gear" )
+          q-list
+            q-item(clickable v-close-popup to="/Mypage" )
+              q-item-section
+                q-item-label 會員資料
+            q-item(clickable v-close-popup to="/Mypage/Paymentcards")
+              q-item-section
+                q-item-label 付款資訊
+            q-item(clickable v-close-popup to="/Mypage/MypageOrders")
+              q-item-section
+                q-item-label 我的訂單
+            q-item(clickable v-close-popup to="/Mypage/Mypagecart")
+              q-item-section
+                q-item-label 我的購物車
+            q-item(clickable v-close-popup to="/Mypage/Wishlist")
+              q-item-section
+                q-item-label 我的收藏
+            q-item(clickable v-close-popup  @click="logout")
+              q-item-section
+                q-item-label 登出
+
+        q-btn(flat rounded v-if="isLogin && isAdmin" icon="fa-solid fa-hammer" to="/admin" label="管理")
+        q-btn#logoutBtn(flat rounded v-if="isLogin && isAdmin" icon="fa-solid fa-right-from-bracket" @click="logout" label="登出")
+
+      //- q-btn(dense flat round icon="menu" @click="toggleLeftDrawer")
       //-   q-avatar
       //-     img(src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg")
       //-   q-toolbar-title(align="left") ~快樂的地方~
@@ -64,6 +85,10 @@ const { logout } = user
 const showRegister = ref(false)
 
 const leftDrawerOpen = ref(false)
+
+function onItemClick () {
+  console.log('Clicked on an Item')
+}
 
 // const toggleLeftDrawer = () => {
 //   leftDrawerOpen.value = !leftDrawerOpen.value
